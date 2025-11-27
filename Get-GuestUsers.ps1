356c4312b3c46c1a62218b7dbd6ef4bf6ad61022
dhkim3 <dhkim3@visang.com>
2025-11-27 16:39:50 +0900

    Update Get-GuestUsers.ps1

--- Get-GuestUsers.ps1
+++ Get-GuestUsers.ps1
@@ -1,38 +1,43 @@
 #requires -Version 5.1
 
-# 모듈 설치
+# 모듈 설치 (GitHub Actions 환경에서 매번 설치)
 Install-Module -Name ImportExcel -Force -Scope CurrentUser
 Install-Module -Name Microsoft.Graph -Force -Scope CurrentUser
+Install-Module -Name Microsoft.Graph.Auth -Force -Scope CurrentUser
 
-# 로그인
-# Microsoft.Graph.Auth 모듈 필요
-Import-Module Microsoft.Graph.Auth
-
-# ClientSecretCredential 객체 생성
+# ClientSecretCredential 객체 생성 (방법 A)
 $ClientSecretCred = [Microsoft.Graph.Auth.ClientSecretCredential]::new(
     $env:AZURE_TENANT_ID,
     $env:AZURE_CLIENT_ID,
     $env:AZURE_CLIENT_SECRET
 )
 
-# Graph 연결
+# Microsoft Graph 연결
 Connect-MgGraph -ClientSecretCredential $ClientSecretCred -Scopes "User.Read.All","AuditLog.Read.All"
 
-
-# 파일명
+# Excel 파일명
 $ts = Get-Date -Format "yyyyMMdd_HHmmss"
 $OutXlsx = "GuestUsers_$ts.xlsx"
 
 # Guest 계정 조회
-$users = Get-MgUser -Filter "userType eq 'Guest'" -All -Property "displayName,mail,userPrincipalName,id,signInActivity"
+$users = Get-MgUser -Filter "userType eq 'Guest'" -All -Property "displayName,mail,otherMails,userPrincipalName,id,signInActivity"
 
-# 가공
+# 데이터 가공
 $rows = $users | ForEach-Object {
-    $email = if ($_.Mail) { $_.Mail } else { $_.UserPrincipalName }
+    $email = if ($_.Mail) { $_.Mail } elseif ($_.OtherMails.Count) { $_.OtherMails[0] } else { $_.UserPrincipalName }
     $s = $_.SignInActivity
+
     $lastInteractive = if ($s -and $s.LastSuccessfulSignInDateTime) { [datetime]$s.LastSuccessfulSignInDateTime } else { $null }
     $lastNonInteractive = if ($s -and $s.LastNonInteractiveSignInDateTime) { [datetime]$s.LastNonInteractiveSignInDateTime } else { $null }
-    $latestLogin = if ($lastInteractive -and $lastNonInteractive) { ($lastInteractive, $lastNonInteractive | Sort-Object -Descending)[0] } elseif ($lastInteractive) { $lastInteractive } elseif ($lastNonInteractive) { $lastNonInteractive } else { $null }
+
+    $latestLogin = if ($lastInteractive -and $lastNonInteractive) {
+        ($lastInteractive, $lastNonInteractive | Sort-Object -Descending)[0]
+    } elseif ($lastInteractive) {
+        $lastInteractive
+    } elseif ($lastNonInteractive) {
+        $lastNonInteractive
+    } else { $null }
+
     [pscustomobject]@{
         Name = $_.DisplayName
         Email = $email
@@ -43,6 +48,5 @@ $rows = $users | ForEach-Object {
     }
 }
 
-# 엑셀 내보내기
+# Excel로 내보내기
 $rows | Export-Excel -Path $OutXlsx -AutoSize -FreezeTopRow -BoldTopRow -WorksheetName 'GuestUsers'
-
